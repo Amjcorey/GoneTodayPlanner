cd3c2e7c06b0b207cd0ba26c88658397d9468725
Amanda Corey <76081915+Amjcorey@users.noreply.github.com>
2024-05-07 12:17:37 -0700

    rework js with cleaner code and refactored with jquery. Update HTML alongside JS. Update readme

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # GoneTodayPlanner
  A daily planner application that allows the user to add events or tasks during a standard work day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
- # Deployments
- GitHub: https://amjcorey.github.io/GoneTodayPlanner/
+ # Deployed URL
+ GitHub Pages: https://amjcorey.github.io/GoneTodayPlanner/
 
-# Credits
+ ## Video
+https://drive.google.com/file/d/1NM6u13eoE9WGQ6lRZCqcuChQXn71kVgv/view
+Display basic use of app.
+
+## Credits
 Starter code (https://github.com/coding-boot-camp/crispy-octo-meme)
 Collaborate with class peer Coral Darling (https://github.com/CoralDarling/Work-Day-Scheduler.git)
-Refernce Bootcamp alumni code (https://github.com/comp7589/WorkDayScheduler.git)
+Refernce Bootcamp alumni code (https://github.com/rmessett15/Work-Day-Scheduler)
--- src/script.js
+++ src/script.js
@@ -1,74 +1,108 @@
 // When document is ready
 $(document).ready(function () {
-  let currentHour = dayjs().hour(); // Get current hour using dayjs
+  // Get current hour using dayjs and display in header
+  let currentHour = dayjs().hour();
 
-  //Display current date in header
-  function displayDate() {
-    let date = dayjs().format("dddd, MMM D YYYY");
-    $("#currentDay").text(date);
-    // console.log(date);
+  // Grabs all save button elements on the html so they can be manipulated in the DOM
+  let saveBtnElement = document.getElementsByClassName("saveBtn");
+
+  // Add event listener ("click") to the save button element which iterates over each time slot
+  for (let i = 0; i < saveBtnElement.length; i++) {
+    saveBtnElement[i].addEventListener("click", saveToDoToLocalStorage);
   }
 
-  // Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage.
+  // Selects the span tag element on the html so it can be manipulated in the DOM
+  let messageSaved = document.getElementById("messageSaved");
 
-  $(".saveBtn").on("click", function () {
-    //get nearby values
-    console.log(this);
-    let text = $(this).siblings(".description").val();
-    let time = $(this).parent().attr("id");
+  // Function which allows user data to be saved to local storage
+  function saveToDoToLocalStorage(event) {
+    let toDo = this.previousSibling.previousSibling.value;
+    let parentId = this.parentElement.id;
+    console.log(parentId);
+    localStorage.setItem(parentId, toDo);
 
-    //set items in local storage.
-    localStorage.setItem(time, text);
-  });
-  // Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements.
-  //Storing data/event description in localStorage unpon button click for each hour in 24-hour time
-  $("hour9 .description").val(localStorage.getItem("hour9"));
-  $("hour10 .description").val(localStorage.getItem("hour10"));
-  $("hour11 .description").val(localStorage.getItem("hour11"));
-  $("hour12 .description").val(localStorage.getItem("hour12"));
-  $("hour13 .description").val(localStorage.getItem("hour13"));
-  $("hour14 .description").val(localStorage.getItem("hour14"));
-  $("hour15 .description").val(localStorage.getItem("hour15"));
-  $("hour16 .description").val(localStorage.getItem("hour16"));
-  $("hour17 .description").val(localStorage.getItem("hour17"));
+    // Message displayed via span tag when save button is clicked
+    messageSaved.textContent = "Your event has been saved to local storage ✅";
+  }
 
-  // Create and display time blocks
-  function displayTimeBlocks() {
-    let currentTime = dayjs().hour();
+  // Refactored with JavaScript (JQUERY DRAFTS BELOW)
 
-    // Loop over each work hour and create time blocks
-    $(".time-block").each(function () {
-      let timeBlock = parseInt($(this).attr("id").split("hour")[1]);
-      console.log(timeBlock, currentTime);
+  // Grabs the class of time-block elements so they can be manipulated in the DOM
+  let timeBlocks = document.getElementsByClassName("time-block");
 
-      // Add code to apply the past, present, or future class to each time
-      // block by comparing the id to the current hour.
+  // Iterates over the time-block class and compares the current hour to the block hour with the if statement and gives rise to the color attributes the user sees delineating past, present, & future time
+  for (let i = 0; i < timeBlocks.length; i++) {
+    let timeBlock = timeBlocks[i];
+    // Grabs local storage data and appends any stored data to the page on re-load
+    let parentId = timeBlocks[i].getAttribute("id");
+    let textValue = localStorage.getItem(parentId);
+    timeBlocks[i].children[1].value = textValue;
 
-      if (timeBlock < currentTime) {
-        $(this).addClass("past");
-        $(this).removeClass("present");
-        $(this).removeClass("future");
-      } else if (timeBlock === currentTime) {
-        $(this).removeClass("past");
-        $(this).addClass("present");
-        $(this).removeClass("future");
-      } else {
-        $(this).removeClass("past");
-        $(this).removeClass("present");
-        $(this).addClass("future");
-      }
+    // Adds the focus event listener to save button element so when user saves an event they receive a message letting them know their event has been saved
+    timeBlocks[i].children[1].addEventListener("focus", function () {
+      messageSaved.textContent = "";
     });
+
+    // Splits the "hour" at the dash and converts the string leftover to a number so it can be compared to the current hour given by the currentHour letiable
+    let blockHour = parseInt(timeBlock.id.split("-")[1]);
+    console.log(blockHour);
+
+    // Conditional statement which gives rise to the color attributes the user sees delineating past, present, & future time
+    if (blockHour < currentHour) {
+      timeBlock.classList.add("past");
+      timeBlock.classList.remove("present");
+      timeBlock.classList.remove("future");
+    } else if (blockHour === currentHour) {
+      timeBlock.classList.add("present");
+      timeBlock.classList.remove("past");
+      timeBlock.classList.remove("future");
+    } else {
+      timeBlock.classList.add("future");
+      timeBlock.classList.remove("past");
+      timeBlock.classList.remove("present");
+    }
   }
-
-  // Call functions
-  displayTimeBlocks();
-  displayDate();
-
-  // Update the time blocks every minute to check for past, present, or future
-  setInterval(function () {
-    displayTimeBlocks();
-  }, 60000);
 });
+// Old code below is a reference to how one would re-write lines using JQUERY
+
+// Adds past, present, future class to each time block by comparing the id to the current hour using dayjs().hour(). (JQUERY DRAFT 1)
+// REFACTOR INTO JAVASCRIPT!!!!
+// let currentHour = dayjs().hour();
+// $(".time-block").each(function() {
+//   let blockHour = parseInt($(this).attr("id").split("-")[1]);
+//   console.log(blockHour)
+//     console.log($(this).attr("id"));
+
+//     if (blockHour < currentHour) {
+//       $(this).addClass("past").removeClass("present future");
+//     } else if (blockHour === currentHour) {
+//       $(this).addClass("present").removeClass("past future");
+//     } else {
+//       $(this).addClass("future").removeClass("past present");
+//     }
+// })
+
+// function displayTimeBlocks() {
+//   let currentTime = dayjs().hour();
+
+// $(".time-block").each(function () {
+//   let timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+//   console.log(timeBlock, currentTime);
+
+// if (timeBlock < currentTime) {
+//   $(this).addClass("past");
+//   $(this).removeClass("present");
+//   $(this).removeClass("future");
+// } else if (timeBlock === currentTime) {
+//   $(this).removeClass("past");
+//   $(this).addClass("present");
+//   $(this).removeClass("future");
+// } else {
+//   $(this).removeClass("past");
+//   $(this).removeClass("present");
+//   $(this).addClass("future");
+// }
+
+// }
+
+//
